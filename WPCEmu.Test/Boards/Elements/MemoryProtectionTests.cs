d4705dd27cb26d209d18d31e846e5e5b4d84d4ef
jsm174 <jsm174@gmail.com>
2020-09-06 11:50:40 -0400

    Added MemoryProtection and Tests

---
+++ WPCEmu.Test/Boards/Elements/MemoryProtectionTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using WPCEmu.Boards.Elements;
+
+namespace WPCEmu.Test.Boards.Elements
+{
+	[TestFixture]
+	public class MemoryProtectionTests
+	{
+		[Test, Order(1)]
+		public void ShouldGetMemoryProtectionMask_0()
+		{
+			TestContext.WriteLine("memoryProtection, should get memoryProtection mask for 0");
+
+			ushort result = MemoryProtection.getMemoryProtectionMask(0);
+			Assert.AreEqual(0x1000, result);
+		}
+
+		[Test, Order(2)]
+		public void ShouldGetMemoryProtectionMask_1()
+		{
+			TestContext.WriteLine("memoryProtection, should get memoryProtection mask for 1");
+
+			ushort result = MemoryProtection.getMemoryProtectionMask(1);
+			Assert.AreEqual(0x1800, result);
+		}
+
+		[Test, Order(3)]
+		public void ShouldGetMemoryProtectionMask_15()
+		{
+			TestContext.WriteLine("memoryProtection, should get memoryProtection mask for 15");
+
+			ushort result = MemoryProtection.getMemoryProtectionMask(15);
+			Assert.AreEqual(0x1F00, result);
+		}
+
+		[Test, Order(4)]
+		public void ShouldGetMemoryProtectionMask_16()
+		{
+			TestContext.WriteLine("memoryProtection, should get memoryProtection mask for 16");
+
+			ushort result = MemoryProtection.getMemoryProtectionMask(16);
+			Assert.AreEqual(0x2000, result);
+		}
+
+		[Test, Order(5)]
+		public void ShouldGetMemoryProtectionMask_17()
+		{
+			TestContext.WriteLine("memoryProtection, should get memoryProtection mask for 17");
+
+			ushort result = MemoryProtection.getMemoryProtectionMask(17);
+			Assert.AreEqual(0x2800, result);
+		}
+
+		[Test, Order(6)]
+		public void ShouldGetMemoryProtectionMask_255_unsure()
+		{
+			TestContext.WriteLine("memoryProtection, should get memoryProtection mask for 255 - unsure");
+
+			ushort result = MemoryProtection.getMemoryProtectionMask(255);
+			Assert.AreEqual(0xF00, result);
+		}
+
+		[Test, Order(7)]
+		public void ShouldGetMemoryProtectionMask_256_wraparound()
+		{
+			TestContext.WriteLine("memoryProtection, should get memoryProtection mask for 256 (wrap around)");
+
+			ushort result = MemoryProtection.getMemoryProtectionMask(256);
+			Assert.AreEqual(0x1000, result);
+		}
+	}
+}
---
+++ WPCEmu/Boards/Elements/MemoryProtection.cs
@@ -0,0 +1,13 @@
+﻿namespace WPCEmu.Boards.Elements
+{
+    public static class MemoryProtection
+    {
+        // calculate memory protection, ripped from pinmame
+        readonly static byte[] SWAP_NIBBLE = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
+
+        public static ushort getMemoryProtectionMask(ushort value)
+        {
+            return (ushort) (0xFFFF & (SWAP_NIBBLE[value & 0x0F] + (value & 0xF0) + 0x10) << 8);
+        }
+    }
+}

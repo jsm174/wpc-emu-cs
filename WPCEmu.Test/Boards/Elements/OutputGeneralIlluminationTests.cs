9c042e9cfe20a5e884ebcf4f603ac841cf6e232b
jsm174 <jsm174@gmail.com>
2020-09-27 08:56:32 -0400

    Added Output General Illumination and Tests

---
+++ WPCEmu.Test/Boards/Elements/OutputGeneralIlluminationTests.cs
@@ -0,0 +1,144 @@
+﻿using NUnit.Framework;
+using WPCEmu.Boards.Elements;
+
+namespace WPCEmu.Test.Boards.Elements
+{
+	[TestFixture]
+	public class OutputGeneralIlluminationTests
+	{
+		OutputGeneralIllumination preWpc95;
+		OutputGeneralIllumination wpc95;
+
+		[SetUp]
+		public void Init()
+		{
+			preWpc95 = OutputGeneralIllumination.GetInstance(false);
+			wpc95 = OutputGeneralIllumination.GetInstance(true);
+		}
+
+		[Test, Order(1)]
+		public void UpdateWithValue0()
+		{
+			TestContext.WriteLine("generalIllumination, update with value 0");
+
+			var generalIllumination = preWpc95;
+			generalIllumination.update(0x0);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[0]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[1]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[2]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[3]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[4]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[5]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[6]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[7]);
+		}
+
+		[Test, Order(2)]
+		public void Wpc95UpdateWithValue0()
+		{
+			TestContext.WriteLine("generalIllumination wpc95, update with value 0");
+
+			var generalIllumination = wpc95;
+			generalIllumination.update(0x0);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[0]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[1]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[2]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[3]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[4]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[5]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[6]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[7]);
+		}
+
+		[Test, Order(3)]
+		public void UpdateWithValue0xFF()
+		{
+			TestContext.WriteLine("generalIllumination, update with value 0xFF");
+
+			var generalIllumination = preWpc95;
+			generalIllumination.update(0xFF);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[0]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[1]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[2]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[3]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[4]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[5]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[6]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[7]);
+		}
+
+		[Test, Order(4)]
+		public void Wpc95UpdateWithValue0xFF()
+		{
+			TestContext.WriteLine("generalIllumination wpc95, update with value 0xFF");
+
+			var generalIllumination = wpc95;
+			generalIllumination.update(0xFF);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[0]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[1]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[2]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[3]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[4]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[5]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[6]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[7]);
+		}
+
+		[Test, Order(5)]
+		public void UpdateWithValue0x2_0x4()
+		{
+			TestContext.WriteLine("generalIllumination, update with value 0x2 and 0x4");
+
+			var generalIllumination = preWpc95;
+			generalIllumination.update(0x2);
+			generalIllumination.update(0x4);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[0]);
+			Assert.AreEqual(0x06, generalIllumination.generalIlluminationState[1]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[2]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[3]);
+		}
+
+		[Test, Order(6)]
+		public void UpdateWithValue0x4_0x4()
+		{
+			TestContext.WriteLine("generalIllumination, update with value 0x4 and 0x4");
+
+			var generalIllumination = preWpc95;
+			generalIllumination.update(0x4);
+			generalIllumination.update(0x4);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[0]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[1]);
+			Assert.AreEqual(0x07, generalIllumination.generalIlluminationState[2]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[3]);
+		}
+
+		[Test, Order(7)]
+		public void GetUint8ArrayFromState()
+		{
+			TestContext.WriteLine("generalIllumination, getUint8ArrayFromState");
+
+			var generalIllumination = preWpc95;
+			var result = generalIllumination.getUint8ArrayFromState(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF });
+			Assert.AreEqual(new byte[] { 8, 8, 8, 8, 8, 8, 8, 8 }, result);
+		}
+
+		[Test, Order(8)]
+		public void ShouldDecreateValueDim()
+		{
+			TestContext.WriteLine("generalIllumination, should decrease value (dim)");
+
+			var generalIllumination = preWpc95;
+			generalIllumination.update(0xFF);
+			generalIllumination.update(0x00);
+			generalIllumination.update(0x00);
+			Assert.AreEqual(0x05, generalIllumination.generalIlluminationState[0]);
+			Assert.AreEqual(0x05, generalIllumination.generalIlluminationState[1]);
+			Assert.AreEqual(0x05, generalIllumination.generalIlluminationState[2]);
+			Assert.AreEqual(0x05, generalIllumination.generalIlluminationState[3]);
+			Assert.AreEqual(0x05, generalIllumination.generalIlluminationState[4]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[5]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[6]);
+			Assert.AreEqual(0x00, generalIllumination.generalIlluminationState[7]);
+		}
+	}
+}
---
+++ WPCEmu/Boards/Elements/OutputGeneralIllumination.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System.Diagnostics;
+
+namespace WPCEmu.Boards.Elements
+{
+    public class OutputGeneralIllumination
+    {
+        public static OutputGeneralIllumination GetInstance(bool isWpc95)
+        {
+            return new OutputGeneralIllumination(isWpc95);
+        }
+
+        // general illumination supports up to 5 lamp groups, Coin door enable relay (Bit 5), Flipper enable relay (Bit 7)
+        // TODO: brightness level 7+8 do not work, no IRQ count for those entries
+        const byte MATRIX_COLUMN_SIZE = 8;
+        const byte ALL_LAMPS_OFF = 0x00;
+        const byte WPC95_TRIAC_MASK = 0xE7;
+        const byte COIN_DOOR_ENABLE_BIT = 5;
+        const byte UNKNOWN_BIT = 6;
+        const byte FLIPPER_RELAY_BIT = 7;
+
+        public byte[] generalIlluminationState;
+        bool isWpc95;
+
+        public OutputGeneralIllumination(bool isWpc95)
+        {
+            generalIlluminationState = Enumerable.Repeat(ALL_LAMPS_OFF, MATRIX_COLUMN_SIZE).ToArray();
+            this.isWpc95 = isWpc95;
+        }
+
+        public void update(byte value, byte irqCountGI = 0)
+        {
+            // WPC-95 only controls 3 of the 5 Triacs, the other 2 are ALWAYS ON, power wired directly
+            byte normalizedValue = isWpc95 ? (byte) ((value & WPC95_TRIAC_MASK) | 0x18) : value;
+            Debug.Print("UPDATE_TRIAC {0} {1}", normalizedValue, irqCountGI);
+
+            for (byte i = 0; i < 5; i++)
+            {
+                if ((normalizedValue & (1 << i)) != 0)
+                {
+                    if (generalIlluminationState[i] < 7)
+                    {
+                        generalIlluminationState[i] = (byte) (7 - irqCountGI);
+                    }
+                    else
+                    {
+                        generalIlluminationState[i] = 7;
+                    }
+                }
+                else if (generalIlluminationState[i] != 0)
+                {
+                    this.generalIlluminationState[i]--;
+                }
+            }
+
+            generalIlluminationState[COIN_DOOR_ENABLE_BIT] = (byte) (((normalizedValue & (1 << COIN_DOOR_ENABLE_BIT)) != 0) ? 7 : 0);
+            generalIlluminationState[UNKNOWN_BIT] = (byte) (((normalizedValue & (1 << UNKNOWN_BIT)) != 0) ? 7 : 0);
+            generalIlluminationState[FLIPPER_RELAY_BIT] = (byte) (((normalizedValue & (1 << FLIPPER_RELAY_BIT)) != 0) ? 7 : 0);
+        }
+
+        byte[] getNormalizedState()
+        {
+            return generalIlluminationState.Select(value => (byte) (value * 31)).ToArray();
+        }
+
+        public byte[] getUint8ArrayFromState(byte[] giState)
+        {
+            byte[] normalizedGiState = giState.Select((value) => (byte) (value / 31)).ToArray();
+            return normalizedGiState;
+        }
+    }
+}

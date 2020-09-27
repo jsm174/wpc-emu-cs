ad081f3a079c4c524259a639e193d4f0107a661e
jsm174 <jsm174@gmail.com>
2020-09-27 13:53:28 -0400

    Added Output Solenoid Matrix and Tests

---
+++ WPCEmu.Test/Boards/Elements/OutputSolenoidMatrixTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using WPCEmu.Boards.Elements;
+
+namespace WPCEmu.Test.Boards.Elements
+{
+	[TestFixture]
+	public class OutputSolenoidMatrixTests
+	{
+		const int UPDATE_AFTER_TICKS = 8;
+
+		OutputSolenoidMatrix solenoidMatrix;
+		
+		[SetUp]
+		public void Init()
+		{
+			solenoidMatrix = OutputSolenoidMatrix.GetInstance(UPDATE_AFTER_TICKS);
+		}
+
+		[Test, Order(1)]
+		public void UpdateAllHighPowerSolenoids()
+		{
+			TestContext.WriteLine("solenoidMatrix, update all high power solenoids");
+
+			solenoidMatrix.write(0x3FE1, 0xFF);
+			Assert.AreEqual(0xFF, solenoidMatrix.solenoidState[0]);
+			Assert.AreEqual(0xFF, solenoidMatrix.solenoidState[7]);
+			Assert.AreEqual(0, solenoidMatrix.solenoidState[8]);
+		}
+
+		[Test, Order(2)]
+		public void UpdateAllLowPowerSolenoids()
+		{
+			TestContext.WriteLine("solenoidMatrix, update all low power solenoids");
+
+			solenoidMatrix.write(0x3FE3, 0xFF);
+			Assert.AreEqual(0, solenoidMatrix.solenoidState[7]);
+			Assert.AreEqual(0xFF, solenoidMatrix.solenoidState[8]);
+			Assert.AreEqual(0xFF, solenoidMatrix.solenoidState[15]);
+		}
+
+		[Test, Order(3)]
+		public void UpdateAllFlashlightSolenoids()
+		{
+			TestContext.WriteLine("solenoidMatrix, update all flashlight solenoids");
+
+			solenoidMatrix.write(0x3FE2, 0xFF);
+			Assert.AreEqual(0, solenoidMatrix.solenoidState[15]);
+			Assert.AreEqual(0xFF, solenoidMatrix.solenoidState[16]);
+			Assert.AreEqual(0xFF, solenoidMatrix.solenoidState[23]);
+		}
+
+		[Test, Order(4)]
+		public void UpdateAllGenericSolenoids()
+		{
+			TestContext.WriteLine("solenoidMatrix, update all generic solenoids");
+
+			solenoidMatrix.write(0x3FE0, 0xFF);
+			Assert.AreEqual(0, solenoidMatrix.solenoidState[23]);
+			Assert.AreEqual(0xFF, solenoidMatrix.solenoidState[24]);
+			Assert.AreEqual(0xFF, solenoidMatrix.solenoidState[31]);
+		}
+
+		[Test, Order(5)]
+		public void FailIfValueExceedsUnsignedByteRange()
+		{
+			TestContext.WriteLine("solenoidMatrix, fail if value exceeds unsigned byte range");
+
+			Assert.Throws<System.Exception>(() => solenoidMatrix.write(0x3FE0, 0xFFF));
+		}
+
+		[Test, Order(6)]
+		public void FailIfAddressIsInvalid()
+		{
+			TestContext.WriteLine("solenoidMatrix, fail if address is invalid");
+
+			Assert.Throws<System.Exception>(() => solenoidMatrix.write(0, 0xFF));
+		}
+
+		[Test, Order(7)]
+		public void UpdateCycles()
+		{
+			TestContext.WriteLine("solenoidMatrix, update cycles");
+
+			solenoidMatrix.write(0x3FE1, 0xFF);
+			solenoidMatrix.executeCycle(UPDATE_AFTER_TICKS);
+			Assert.AreEqual(0x7F, solenoidMatrix.solenoidState[0]);
+		}
+	}
+}
---
+++ WPCEmu/Boards/Elements/OutputSolenoidMatrix.cs
@@ -0,0 +1,118 @@
+﻿using System.Linq;
+using System.Diagnostics;
+using System;
+
+/*
+All versions of the power driver board support 28 controlled outputs for
+solenoids, motors, etc.  These are divided into four groups:
+8 high power drivers, 8 low power drivers, 8 flashlamp drivers, and
+4 general purpose drivers.  Each bank operates at a different voltage,
+somewhere between 20V and 50V.
+The CPU board enables/disable a driver by writing a command to the
+power driver board.  All values are latched on the driver board and thus
+retain their states until the CPU changes them.  The latches are not
+readable, so software must maintain the last value written in RAM.
+A CPU board reset will assert a blanking signal to reset the latches;
+this helps in the event of a software crash.
+TODO On WPC-95, 4 additional low voltage outputs running at 5V are added
+to the general purpose group, which can be used for miscellaneous I/O
+like small DC motors.
+$3FE0     Byte     WPC_SOLENOID_GEN_OUTPUT (7-0: W: Enables for solenoids 25-29) or 25-28???
+$3FE1     Byte     WPC_SOLENOID_HIGHPOWER_OUTPUT (7-0: W: Enables for solenoids 1-8)
+$3FE2     Byte     WPC_SOLENOID_FLASH1_OUTPUT (7-0: W: Enables for solenoids 17-28)
+$3FE3     Byte     WPC_SOLENOID_LOWPOWER_OUTPUT (7-0: W: Enables for solenoids 9-16)
+*/
+
+namespace WPCEmu.Boards.Elements
+{
+    public class OutputSolenoidMatrix
+    {
+        public static OutputSolenoidMatrix GetInstance(int updateAfterTicks)
+        {
+            return new OutputSolenoidMatrix(updateAfterTicks);
+        }
+
+        const byte NUMBER_OF_SOLENOIDS = 48;
+        const byte ALL_SOLENOID_OFF = 0x00;
+
+        const ushort WPC_SOLENOID_GEN_OUTPUT = 0x3FE0;
+        const ushort WPC_SOLENOID_HIGHPOWER_OUTPUT = 0x3FE1;
+        const ushort WPC_SOLENOID_FLASH1_OUTPUT = 0x3FE2;
+        const ushort WPC_SOLENOID_LOWPOWER_OUTPUT = 0x3FE3;
+
+        const byte OFFSET_SOLENOID_HIGHPOWER = 0;
+        const byte OFFSET_SOLENOID_LOWPOWER = 8;
+        const byte OFFSET_SOLENOID_FLASHLIGHT = 16;
+        const byte OFFSET_SOLENOID_GENERIC = 24;
+        const byte OFFSET_SOLENOID_FLIPTRONIC = 32;
+
+        int updateAfterTicks;
+        public byte[] solenoidState;
+        int ticks;
+
+        public OutputSolenoidMatrix(int updateAfterTicks)
+        {
+            if (updateAfterTicks == 0)
+            {
+                throw new Exception("MISSING_UPDATE_AFTER_TICKS");
+            }
+            this.updateAfterTicks = updateAfterTicks;
+            solenoidState = Enumerable.Repeat(ALL_SOLENOID_OFF, NUMBER_OF_SOLENOIDS).ToArray();
+            ticks = 0;
+        }
+
+        void _updateSolenoidsPacked(byte offset, byte value)
+        {
+            for (byte i = 0; i < 8; i++)
+            {
+                if ((value & (1 << i)) != 0)
+                {
+                    solenoidState[offset + i] = 0xFF;
+                }
+            }
+        }
+
+        public void executeCycle(int ticks)
+        {
+            this.ticks += ticks;
+            // output solenoids state @ 60hz/8 - TODO
+            if (this.ticks >= updateAfterTicks)
+            {
+                Debug.Print("update solenoids state");
+                this.ticks -= updateAfterTicks;
+                solenoidState = solenoidState.Select((state) => (byte)(state >> 1)).ToArray();
+            }
+        }
+
+        void writeFliptronic(byte value)
+        {
+            Debug.Print("UPDATE_FLIPPER_SOLENOIDS");
+            _updateSolenoidsPacked(OFFSET_SOLENOID_FLIPTRONIC, value);
+        }
+
+        public void write(ushort sourceAddress, ushort value)
+        {
+            if (value < 0 || value > 0xFF)
+            {
+                throw new Exception("SOLENOID_MATRIX_INVALID_VALUE_" + value);
+            }
+            switch (sourceAddress)
+            {
+                case WPC_SOLENOID_HIGHPOWER_OUTPUT:
+                    _updateSolenoidsPacked(OFFSET_SOLENOID_HIGHPOWER, (byte)value);
+                    break;
+                case WPC_SOLENOID_LOWPOWER_OUTPUT:
+                    _updateSolenoidsPacked(OFFSET_SOLENOID_LOWPOWER, (byte)value);
+                    break;
+                case WPC_SOLENOID_FLASH1_OUTPUT:
+                    _updateSolenoidsPacked(OFFSET_SOLENOID_FLASHLIGHT, (byte)value);
+                    break;
+                case WPC_SOLENOID_GEN_OUTPUT:
+                    _updateSolenoidsPacked(OFFSET_SOLENOID_GENERIC, (byte)value);
+                    break;
+                default:
+                    throw new Exception("SOLENOID_MATRIX_INVALID_OFFSET_" + sourceAddress/*.toString(16)*/);
+            }
+        }
+    }
+}
